f286e47d0c9761d85129656b74b8e6907c3f0f4d
Fatih Tashlanov <>
2021-02-01 14:42:10 +0300

    add i18n

---
+++ frontend/next.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//     i18n: {
+//         locales: ["en", "ru", "nl"],
+//         defaultLocale: "ru",
+//         domains: [
+//             {
+//                 domain: "example.com",
+//                 defaultLocale: "en-US",
+//             },
+//             {
+//                 domain: "example.nl",
+//                 defaultLocale: "nl-NL",
+//             },
+//             {
+//                 domain: "example.fr",
+//                 defaultLocale: "fr",
+//             },
+//         ],
+//     },
+// };
